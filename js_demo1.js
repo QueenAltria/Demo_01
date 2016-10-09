69ac8206f0401767cd725956c50399a57f71485c
QueenAltria <471040339@qq.com>
2016-10-09 23:18:28 +0800

    Initial commit

---
+++ js_demo1.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Jiang on 2016/10/9.
+ */
+//alert("你好啊")
+var i=0;
+for (i=0;i<=10;i++)
+{
+    if(i==3){
+        continue;
+    }
+    document.write("The number is "+i);
+    document.write("<br/>")
+}
